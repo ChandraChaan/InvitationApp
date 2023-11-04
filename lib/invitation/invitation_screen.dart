2c8c96d6fab689ff53813a07355b65703450352b
Chandra <dchandrachaan@gmail.com>
2023-11-04 10:24:32 +0530

    color changes done

--- lib/invitation/invitation_screen.dart
+++ lib/invitation/invitation_screen.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 
-class InvitationScreen extends StatelessWidget {
+class InvitationScreen extends StatefulWidget {
+  @override
+  State<InvitationScreen> createState() => _InvitationScreenState();
+}
+
+class _InvitationScreenState extends State<InvitationScreen> {
+  ScrollController _scrollController = ScrollController();
+  bool _isWidgetVisible = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      if (_scrollController.offset > 100) {
+        // Adjust the offset value as needed
+        setState(() {
+          _isWidgetVisible = false;
+        });
+      } else {
+        setState(() {
+          _isWidgetVisible = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          // Background widget
           Container(
             width: double.infinity,
             height: double.infinity,
@@ -18,7 +48,57 @@ class InvitationScreen extends StatelessWidget {
               ),
             ),
           ),
-          // Scrollable ListView
+          if (_isWidgetVisible)
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Container(
+                  //todo conditional color
+                  color: Colors.white,
+                  child: Container(
+                    //todo conditional color
+                    color: Colors.green.shade100,
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          ' ',
+                          style: Theme.of(context)
+                              .textTheme
+                              .displayLarge
+                              ?.copyWith(color: Colors.white),
+                        ),
+                        const SizedBox(height: 8.0),
+                        const Text(
+                          ' ',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                        const SizedBox(height: 8.0),
+                        Text(
+                          'Swetha',
+                          style: Theme.of(context)
+                              .textTheme
+                              .displayLarge
+                              ?.copyWith(color: Colors.white),
+                        ),
+                        const SizedBox(height: 32.0),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
           Positioned(
             top: 0,
             left: 0,
@@ -31,8 +111,10 @@ class InvitationScreen extends StatelessWidget {
                 child: Column(
                   children: [
                     Container(
+                      //todo conditional color
                       color: Colors.white,
                       child: Container(
+                        //todo conditional color
                         color: Colors.green,
                         height: MediaQuery.of(context).size.height,
                         width: MediaQuery.of(context).size.width,
@@ -56,6 +138,7 @@ class InvitationScreen extends StatelessWidget {
                               ),
                             ),
                             const SizedBox(height: 8.0),
+                            if (_isWidgetVisible == false)
                             Text(
                               'Swetha',
                               style: Theme.of(context)
