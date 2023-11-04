47ede97771cf23641f6d28ce7e445a648df8c217
Chandra <dchandrachaan@gmail.com>
2023-11-04 10:30:46 +0530

    color changes done

--- lib/invitation/invitation_screen.dart
+++ lib/invitation/invitation_screen.dart
@@ -8,6 +8,7 @@ class InvitationScreen extends StatefulWidget {
 class _InvitationScreenState extends State<InvitationScreen> {
   ScrollController _scrollController = ScrollController();
   bool _isWidgetVisible = true;
+  double _widgetOpacity = 1.0;
 
   @override
   void initState() {
@@ -18,11 +19,18 @@ class _InvitationScreenState extends State<InvitationScreen> {
         setState(() {
           _isWidgetVisible = false;
         });
-      } else {
+      }
+      else {
         setState(() {
           _isWidgetVisible = true;
         });
       }
+
+      setState(() {
+        _widgetOpacity = 1.0 - (_scrollController.offset / 200.0);
+        // 200.0 is the threshold where the widget will become completely transparent
+        _widgetOpacity = _widgetOpacity.clamp(0.0, 1.0);
+      });
     });
   }
 
@@ -35,20 +43,78 @@ class _InvitationScreenState extends State<InvitationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            alignment: Alignment.center,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/wallpaper.jpeg'),
-                fit: BoxFit.cover,
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (scrollNotification) {
+          // Update the scrollController
+          if (scrollNotification is ScrollUpdateNotification) {
+            _scrollController.jumpTo(scrollNotification.metrics.pixels);
+          }
+          return true;
+        },
+        child: Stack(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              alignment: Alignment.center,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/wallpaper.jpeg'),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          if (_isWidgetVisible)
+            if (_isWidgetVisible)
+              Positioned(
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: Container(
+                    //todo conditional color
+                    // color: Colors.white.withOpacity(_widgetOpacity),
+                    child: Container(
+                      //todo conditional color
+                      color: Colors.green.withOpacity(_widgetOpacity),
+                      height: MediaQuery.of(context).size.height,
+                      width: MediaQuery.of(context).size.width,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            ' ',
+                            style: Theme.of(context)
+                                .textTheme
+                                .displayLarge
+                                ?.copyWith(color: Colors.white),
+                          ),
+                          const SizedBox(height: 8.0),
+                          const Text(
+                            ' ',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontStyle: FontStyle.italic,
+                            ),
+                          ),
+                          const SizedBox(height: 8.0),
+                          Text(
+                            'Swetha',
+                            style: Theme.of(context)
+                                .textTheme
+                                .displayLarge
+                                ?.copyWith(color: Colors.white),
+                          ),
+                          const SizedBox(height: 32.0),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             Positioned(
               top: 0,
               left: 0,
@@ -57,111 +123,63 @@ class _InvitationScreenState extends State<InvitationScreen> {
               child: SizedBox(
                 height: MediaQuery.of(context).size.height,
                 width: MediaQuery.of(context).size.width,
-                child: Container(
-                  //todo conditional color
-                  color: Colors.white,
-                  child: Container(
-                    //todo conditional color
-                    color: Colors.green.withOpacity(1.0),
-                    height: MediaQuery.of(context).size.height,
-                    width: MediaQuery.of(context).size.width,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          ' ',
-                          style: Theme.of(context)
-                              .textTheme
-                              .displayLarge
-                              ?.copyWith(color: Colors.white),
-                        ),
-                        const SizedBox(height: 8.0),
-                        const Text(
-                          ' ',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            fontStyle: FontStyle.italic,
-                          ),
-                        ),
-                        const SizedBox(height: 8.0),
-                        Text(
-                          'Swetha',
-                          style: Theme.of(context)
-                              .textTheme
-                              .displayLarge
-                              ?.copyWith(color: Colors.white),
-                        ),
-                        const SizedBox(height: 32.0),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Container(
-                      //todo conditional color
-                      // color: Colors.white,
-                      child: Container(
+                child: SingleChildScrollView(
+                  controller: _scrollController,
+                  child: Column(
+                    children: [
+                      Container(
                         //todo conditional color
-                        // color: Colors.green,
-                        height: MediaQuery.of(context).size.height,
-                        width: MediaQuery.of(context).size.width,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Chandra ObulReddy',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .displayLarge
-                                  ?.copyWith(color: Colors.white),
-                            ),
-                            const SizedBox(height: 8.0),
-                            const Text(
-                              'Weds',
-                              style: TextStyle(
-                                fontSize: 18.0,
-                                fontStyle: FontStyle.italic,
-                              ),
-                            ),
-                            const SizedBox(height: 8.0),
-                            if (_isWidgetVisible == false)
+                        // color: Colors.white,
+                        child: Container(
+                          //todo conditional color
+                          // color: Colors.green,
+                          height: MediaQuery.of(context).size.height,
+                          width: MediaQuery.of(context).size.width,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
                               Text(
-                                'Swetha',
+                                'Chandra ObulReddy',
                                 style: Theme.of(context)
                                     .textTheme
                                     .displayLarge
                                     ?.copyWith(color: Colors.white),
                               ),
-                            const SizedBox(height: 32.0),
-                          ],
+                              const SizedBox(height: 8.0),
+                              const Text(
+                                'Weds',
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                              const SizedBox(height: 8.0),
+                              if (_isWidgetVisible == false)
+                                Text(
+                                  'Swetha',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .displayLarge
+                                      ?.copyWith(color: Colors.white),
+                                ),
+                              const SizedBox(height: 32.0),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    Container(
-                      color: Colors.transparent,
-                      height: MediaQuery.of(context).size.height,
-                      width: MediaQuery.of(context).size.width,
-                    ),
-                  ],
+                      Container(
+                        color: Colors.transparent,
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
