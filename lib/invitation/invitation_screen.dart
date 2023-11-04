4fc3d13368f074f663e4aac31b6876137c66420b
Chandra <dchandrachaan@gmail.com>
2023-11-04 09:44:37 +0530

    invitation screen added

--- lib/invitation/invitation_screen.dart
+++ lib/invitation/invitation_screen.dart
@@ -4,40 +4,81 @@ class InvitationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black38,
-      body: Container(
-        padding: const EdgeInsets.all(16.0),
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Chandra ObulReddy',
-              style: Theme.of(context)
-                  .textTheme
-                  .displayLarge
-                  ?.copyWith(color: Colors.white),
-            ),
-            const SizedBox(height: 8.0),
-            const Text(
-              'Weds',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontStyle: FontStyle.italic,
+      body: Stack(
+        children: <Widget>[
+          // Background widget
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            alignment: Alignment.center,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/wallpaper.jpeg'),
+                fit: BoxFit.cover,
               ),
             ),
-            const SizedBox(height: 8.0),
-            Text(
-              'Swetha',
-              style: Theme.of(context)
-                  .textTheme
-                  .displayLarge
-                  ?.copyWith(color: Colors.white),
+          ),
+          // Scrollable ListView
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      color: Colors.white,
+                      child: Container(
+                        color: Colors.green,
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Chandra ObulReddy',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .displayLarge
+                                  ?.copyWith(color: Colors.white),
+                            ),
+                            const SizedBox(height: 8.0),
+                            const Text(
+                              'Weds',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontStyle: FontStyle.italic,
+                              ),
+                            ),
+                            const SizedBox(height: 8.0),
+                            Text(
+                              'Swetha',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .displayLarge
+                                  ?.copyWith(color: Colors.white),
+                            ),
+                            const SizedBox(height: 32.0),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      color: Colors.transparent,
+                      height: MediaQuery.of(context).size.height,
+                      width: MediaQuery.of(context).size.width,
+                    ),
+                  ],
+                ),
+              ),
             ),
-            const SizedBox(height: 32.0),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
