b67adb68db984d34d8103adebfc38ec7b4eda6f6
Chandra <dchandrachaan@gmail.com>
2023-11-04 09:01:19 +0530

    invitation screen added

---
+++ lib/invitation/invitation_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class InvitationScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black38,
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Chandra ObulReddy',
+              style: Theme.of(context)
+                  .textTheme
+                  .displayLarge
+                  ?.copyWith(color: Colors.white),
+            ),
+            const SizedBox(height: 8.0),
+            const Text(
+              'Weds',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Text(
+              'Swetha',
+              style: Theme.of(context)
+                  .textTheme
+                  .displayLarge
+                  ?.copyWith(color: Colors.white),
+            ),
+            const SizedBox(height: 32.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
